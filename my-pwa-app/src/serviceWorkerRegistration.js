cb7997889901739eada845901fd6d1800a5d08fe
Vignesh Ravisankar <vignesh_ravisankar@trimble.com>
2025-03-23 22:09:24 +0530

    minor changes

--- my-pwa-app/src/serviceWorkerRegistration.js
+++ my-pwa-app/src/serviceWorkerRegistration.js
@@ -2,7 +2,7 @@
 export function register(config) {
   if ("serviceWorker" in navigator) {
     window.addEventListener("load", () => {
-      const swUrl = `${process.env.PUBLIC_URL}/service-worker.js`;
+      const swUrl = `/docs/service-worker.js`;
 
       navigator.serviceWorker
         .register(swUrl)
