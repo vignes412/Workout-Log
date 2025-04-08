3139fe9d988d46e882983f4499455e2a912388cf
Vignesh Ravisankar <vignesh_ravisankar@trimble.com>
2025-04-08 07:07:29 +0530

    deployment changes

--- my-pwa-app/src/service-worker.js
+++ my-pwa-app/src/service-worker.js
@@ -26,24 +26,3 @@ self.addEventListener("install", (event) => {
 self.addEventListener("activate", (event) => {
   event.waitUntil(self.clients.claim());
 });
-
-self.addEventListener("message", (event) => {
-  if (event.data && event.data.type === "CACHE_IMAGES") {
-    const imageUrls = event.data.urls;
-    event.waitUntil(
-      caches.open("image-cache").then((cache) => {
-        return Promise.all(
-          imageUrls.map((url) =>
-            cache.match(url).then((response) => {
-              if (!response) {
-                return fetch(url).then((networkResponse) => {
-                  return cache.put(url, networkResponse.clone());
-                });
-              }
-            })
-          )
-        );
-      })
-    );
-  }
-});
